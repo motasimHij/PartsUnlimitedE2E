226186b790bfcf2eb1f8cc9fed92ddcba6fcf2fb
Motasim Hijawy <motasim_mah@hotmail.com>
2020-11-15 15:13:36 +0400

    Add Second change, and push one file

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        //MyFirstChange
+        //MySecondChange
         [Key]
         public int CartItemId { get; set; }
 
